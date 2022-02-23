71eb946fa5b767fd3c38207a797665e5f40204d3
thientt <thientt.bdst@gmail.com>
2022-02-23 19:34:23 +0700

    change something

--- src/App.js
+++ src/App.js
@@ -1,140 +1,130 @@
-import React, { Component } from 'react';
-import Gantt from './components/Gantt';
-import Toolbar from './components/Toolbar';
-import MessageArea from './components/MessageArea';
-import Moment from 'react-moment';
-import 'moment-timezone';
-import moment from 'moment';
-import { gantt } from 'dhtmlx-gantt';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Gantt from "./components/Gantt";
+import Toolbar from "./components/Toolbar";
+import moment from "moment";
 
-
-const data = {
-  data: [],
-  links: []
-}
-const data1 = {
-  data: [],
-  links: []
-}
-
-const getData = async () => {
-  localStorage.removeItem("data")
-  const api = await fetch("http://localhost:1337/data/")
-    .then((response) => { return response.text() })
-    .then(data => {
-      localStorage.setItem("data", data)
-      return data
-    })
-  return api
-}
-
-const logDataUpdate = async (type, action, item, id, parent) => {
-
-  switch (action) {
-    case "create":
-      const createOptions = {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(item)
-      }
-      await fetch("http://localhost:1337/data/task", createOptions)
-      break;
-    case "delete":
-      const deleteOptions = {
-        method: 'DELETE',
-      }
-      await fetch(`http://localhost:1337/data/task/${id}`, deleteOptions)
-      break;
-
-    default:
-      break;
-  }
-  if (action === 'create') {
-    const reqOptions = {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(item)
-    }
-    await fetch("http://localhost:1337/data/task", reqOptions).then(() => getData())
-  }
-}
-
-
-const dataJson = JSON.parse(localStorage.getItem("data"))
-data.data = dataJson ? dataJson.data : []
-console.log(getData());
-data.data.forEach(element => {
-  const start_date = moment(element.start_date).format("YYYY-MM-DD hh:mm:ss")
-  element.start_date = start_date
-});
-// console.log(dataJson.data)
-class App extends Component {
-  state = {
-    currentZoom: 'Days',
-    messages: []
+const App = () => {
+  const refreshSource = () => {
+    const dataJson = JSON.parse(localStorage.getItem("data"));
+    const data = {
+      data: [],
+      links: [],
+    };
+    data.data = dataJson ? dataJson.data : [];
+    data.data.forEach((element) => {
+      const start_date = moment(element.start_date).format(
+        "YYYY-MM-DD hh:mm:ss"
+      );
+      element.start_date = start_date;
+    });
+    return data;
+  };
+  const [sources, setSources] = useState(() => {
+    const data = refreshSource();
+    console.log("data", data);
+    return data;
+  });
+  const getData = async () => {
+    //   localStorage.removeItem("data");
+    const api = await fetch("http://localhost:1337/data/")
+      .then((response) => {
+        return response.text();
+      })
+      .then((res) => {
+        localStorage.setItem("data", res);
+        setSources(() => {
+          return JSON.parse(res);
+        });
+        return res;
+      });
+    return api;
   };
 
+  useEffect(() => {
+    const getData = async () => {
+      //   localStorage.removeItem("data");
+      const api = await fetch("http://localhost:1337/data/")
+        .then((response) => {
+          return response.text();
+        })
+        .then((res) => {
+          localStorage.setItem("data", res);
+          setSources(() => {
+            return JSON.parse(res);
+          });
+          return res;
+        });
+      return api;
+    };
+    getData();
+  }, []);
 
-  addMessage(message) {
-    const maxLogLength = 5;
-    const newMessage = { message };
-    const messages = [
-      newMessage,
-      ...this.state.messages
-    ];
+  console.log("++++++++++++++");
+  console.log(sources.data);
 
-    if (messages.length > maxLogLength) {
-      messages.length = maxLogLength;
+  //   const currentZoom = "Days";
+  const [currentZoom, setCurrentZoom] = useState("Days");
+  const handleZoomChange = (zoom) => {
+    setCurrentZoom(zoom);
+  };
+
+  const logDataUpdate = async (type, action, item, id, parent) => {
+    switch (action) {
+      case "create":
+        console.log("create ======>");
+        console.log(item);
+        const createOptions = {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(item),
+        };
+        await fetch("http://localhost:1337/data/task", createOptions).then(
+          () => {
+            setSources((prev) => {
+              const newSource = getData();
+              return newSource;
+            });
+          }
+        );
+
+        break;
+      case "delete":
+        const deleteOptions = {
+          method: "DELETE",
+        };
+        await fetch(
+          `http://localhost:1337/data/task/${id}`,
+          deleteOptions
+        ).then(() => {
+          setSources(() => {
+            const newSource = getData();
+            return newSource;
+          });
+        });
+        break;
+
+      default:
+        break;
     }
-    this.setState({ messages });
-  }
-
-  logDataUpdate = (type, action, item, id, parent) => {
-    let text = item && item.parent ? ` (${item.parent})` : '';
-    let message = `${type} ${action}: ${id} ${text}`;
-    if (type === 'link' && action !== 'delete') {
-      message += ` ( source: ${item.source}, target: ${item.target} )`;
-    }
-    this.addMessage(message);
-  }
-
-  handleZoomChange = (zoom) => {
-    this.setState({
-      currentZoom: zoom
-    });
-  }
-
-  render() {
-    const { currentZoom, messages } = this.state;
-    return (
-      <div>
-        <div className="zoom-bar">
-          <Toolbar
-            zoom={currentZoom}
-            onZoomChange={this.handleZoomChange}
-          />
-        </div>
-        <div className="gantt-container">
-          <Gantt
-            tasks={data}
-            zoom={currentZoom}
-            onDataUpdated={logDataUpdate}
-
-          />
-        </div>
-
+  };
+  return (
+    <div>
+      <div className="zoom-bar">
+        <Toolbar zoom={currentZoom} onZoomChange={handleZoomChange} />
       </div>
-    );
-  }
-}
+      <div className="gantt-container">
+        <Gantt
+          tasks={sources}
+          zoom={currentZoom}
+          onDataUpdated={logDataUpdate}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
